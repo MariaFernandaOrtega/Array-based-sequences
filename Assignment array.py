d60fe2bf3a3eeb0d46d59d4b3f97d73f821852d3
MariaFernandaOrtega <229757@students.hertie-school.org>
2023-03-05 16:33:30 +0100

    answer

---
+++ Assignment array.py
@@ -0,0 +1,40 @@
+def encrypt(key, message):
+    message = message.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+
+    for letter in message:
+        if letter in alpha:
+            letter_index = (alpha.find(letter) + key) % len(alpha)
+
+            result = result + alpha[letter_index]
+        else:
+            result = result + letter
+
+    return result
+
+def decrypt(key, message):
+    message = message.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+
+    for letter in message:
+        if letter in alpha:
+            letter_index = (alpha.find(letter) - key) % len(alpha)
+
+            result = result + alpha[letter_index]
+        else:
+            result = result + letter
+
+    return result
+
+
+word = "THE EAGLE IS IN PLAY; MEET AT JOE'S"
+
+
+encrypted = encrypt(3,word)
+print("Secret: ",encrypted)
+
+
+decrypted = decrypt(3,encrypted)
+print("Message:",decrypted)
